3cb91e6c5db740923a81d7e151b03633844958da
kimtaekyuni <tgkim2003@gmail.com>
2023-05-20 21:30:01 +0900

    중간정리

--- YonseiChicken/app/views.py
+++ YonseiChicken/app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-
+from .models import Person, CountChicken, Department, User
 # Create your views here.
 def main(request):
-  
   return render(request, 'main.html')
 
 def fur(request):
@@ -21,7 +20,30 @@ def rank(request):
   
   return render(request, 'rank.html')
 
-def signup(request):
-  
-  return render(request, 'signup.html')
+def singup(request):
+  departments = Department.objects.all()
+  if request.method == 'POST':
+    username = request.POST['userid']
+    password = request.POST['userpw']
 
+    exist_user = User.objects.filter(username=username)
+
+    if exist_user:
+      error = "이미 존재하는 아이디입니다."
+      return render(request, 'signup.html', {'error':error, 'departments': departments})
+    
+    new_user = User.objects.create(
+      username = username,
+      password = password
+    )
+    new_department = Department.objects.create(
+      department_name = request.POST['department']
+    ) 
+    Person.objects.create(
+      user = new_user,
+      nickname = request.POST['usernickname'],
+      department = new_department,
+    )
+
+    return redirect('login') 
+  return render(request, 'signup.html', {'departments': departments})
